ca0a5735d226dd6596cbd41d096298c1a1bef725
ElGunner79 <richard.brazao@outlook.com>
2023-11-25 15:50:42 +0800

    Cleaned Code

--- se-course-module-6-lab/src/App.jsx
+++ se-course-module-6-lab/src/App.jsx
@@ -3,23 +3,23 @@ import Calculator from './components/Calculator';
 import BigCats from './components/BigCats';
 import Emoji from './components/Emoji';
 import Greeting from './components/Greeting';
-
-
 function App() {
-
   return (
     <>
+      <h3>Lab 1</h3>
       <Greeting name="Richard">
         <div>Welcome to Saturday Lab 6.</div>
       </Greeting>
-      <br></br>
+      <br />
+      <h3>Lab 2</h3>
       <Emoji />
-      <br></br>
+      <br />
+      <h3>Lab 3/4/5</h3>
       <BigCats />
-      <br></br>
+      <br />
+      <h3>Lab 6</h3>
       <Calculator />
     </>
   )
 }
-
 export default App
