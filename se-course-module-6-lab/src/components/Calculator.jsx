4ad8e1cb8500c18cc49b95bd9a8ae73c019e877f
ElGunner79 <richard.brazao@outlook.com>
2023-11-25 16:11:30 +0800

    Final commit

--- se-course-module-6-lab/src/components/Calculator.jsx
+++ se-course-module-6-lab/src/components/Calculator.jsx
@@ -1,37 +1,32 @@
 import React, { useState } from 'react';
-
 function Calculator() {
     const [firstNumber, setFirstNumber] = useState(0);
     const [secondNumber, setSecondNumber] = useState(0);
     const [operator, setOperator] = useState("+");
     const [result, setResult] = useState(0);
-
     const handleFirstNumberChange = (event) => {
         setFirstNumber(Number(event.target.value));
     };
-
     const handleSecondNumberChange = (event) => {
         setSecondNumber(Number(event.target.value));
     };
-
     const handleOperatorChange = (event) => {
         setOperator(event.target.value);
     };
-
     const handleCalculate = () => {
         const calculation = calculate(firstNumber, secondNumber, operator);
         setResult(calculation);
     };
-
     const handleReset = () => {
         setFirstNumber(0);
         setSecondNumber(0);
         setOperator("+");
         setResult(0);
     };
-
     return (
         <div>
+            <label htmlFor="firstNumber">First number</label>
+            <br />
             <input
                 type="number"
                 placeholder="First number"
@@ -39,12 +34,18 @@ function Calculator() {
                 value={firstNumber}
                 onChange={handleFirstNumberChange}
             />
+            <br />
+            <label htmlFor="operator">Operator</label>
+            <br />
             <select value={operator} onChange={handleOperatorChange}>
                 <option value="+">+</option>
                 <option value="-">-</option>
                 <option value="*">*</option>
                 <option value="/">/</option>
             </select>
+            <br />
+            <label htmlFor="secondNumber">Second number</label>
+            <br />
             <input
                 type="number"
                 placeholder="Second number"
@@ -61,7 +62,6 @@ function Calculator() {
         </div>
     );
 }
-
 function calculate(firstNumber, secondNumber, operator) {
     switch (operator) {
         case "+":
@@ -76,5 +76,4 @@ function calculate(firstNumber, secondNumber, operator) {
             return 0;
     }
 }
-
 export default Calculator;
