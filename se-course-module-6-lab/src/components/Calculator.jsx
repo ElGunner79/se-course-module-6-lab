ebe0f4ad06d0ce827eb1b46d68006acc77d5f558
ElGunner79 <richard.brazao@outlook.com>
2023-11-25 15:41:53 +0800

    Cleaned the code

--- se-course-module-6-lab/src/components/Calculator.jsx
+++ se-course-module-6-lab/src/components/Calculator.jsx
@@ -1,5 +1,5 @@
-
 import React, { useState } from 'react';
+
 function Calculator() {
     const [firstNumber, setFirstNumber] = useState(0);
     const [secondNumber, setSecondNumber] = useState(0);
